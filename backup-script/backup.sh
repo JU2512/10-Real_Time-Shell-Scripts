b7dc1f002d275031955902f7f22bd21254568c2c
Ubuntu <ubuntu@ip-172-31-32-165.ap-south-1.compute.internal>
2025-06-27 05:09:18 +0000

    Add working log backup script

---
+++ backup-script/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source_dir="/var/log"
+destination_dir="/home/ubuntu/log_backups"
+backup_filename="log_backup_$(date +'%Y-%m-%d %H:%M:%S').tar.gz"
+mkdir -p "$destination_dir"
+tar -czvf "$destination_dir/$backup_filename" "$source_dir"
+if [ $? -eq 0 ]; then
+	echo "Backup Successful:$backup_file is created in $destination_dir"
+else
+	echo "Backup Failed"
+fi
