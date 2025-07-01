d4b6154d26e61b3746f197ea482be822a24f554a
JU2512 <jyothiurade874@gmail.com>
2025-07-01 19:15:21 +0530

    Update backup.sh

--- backup-script/backup.sh
+++ backup-script/backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source_dir="/var/log"
 destination_dir="/home/ubuntu/log_backups"
-backup_filename="log_backup_$(date +'%Y-%m-%d %H:%M:%S').tar.gz"
+backup_filename="log_backup_$(date +'%Y-%m-%d_%H-%M-%S').tar.gz"
 mkdir -p "$destination_dir"
 tar -czvf "$destination_dir/$backup_filename" "$source_dir"
 if [ $? -eq 0 ]; then
-	echo "Backup Successful:$backup_file is created in $destination_dir"
+	echo "Backup Successful:$backup_filename is created in $destination_dir"
 else
 	echo "Backup Failed"
 fi
